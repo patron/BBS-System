b276be8688b5687af1358ddc29ad11a1cd837558
Grygorev-udt\Grygorev_o <st.graalex@gmail.com>
2018-10-21 03:06:40 +0300

    small improvement

--- application/views/jqui-bc-manager.php
+++ application/views/jqui-bc-manager.php
@@ -21,7 +21,7 @@
 </script>
 
 
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/plugins/jquery-ui.min.js"></script>
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
 <script>
